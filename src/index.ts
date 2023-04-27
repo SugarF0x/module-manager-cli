063dfc5678fb8da6455d3005013b9527b2f1098c
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-04-27 19:57:08 +0300

    explore prompt chaining

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('test')
+import inquirer from "inquirer"
+
+async function main() {
+  console.clear()
+  const answers1 = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What do you want to do?',
+      choices: [
+        'Create new module',
+        'Extend existing module',
+      ],
+    },
+  ])
+const answer2 = await inquirer.prompt([
+  {
+    type: 'list',
+    name: 'action2',
+    message: 'poop piss?',
+    choices: [
+      'poop',
+      'piss'
+    ],
+  },
+])
+}
+
+main()
